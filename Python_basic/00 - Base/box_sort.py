cc7dd36917b7b8a11310a65dd5a48a23eaf78363
VAS-PC-IDE <vas.ide2022@gmail.com>
2023-02-16 11:19:09 +0300

    Met rebase + ride write

---
+++ Python_basic/00 - Base/box_sort.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+width = 45
+length = 190
+height = 15
+
+
+
+
+if width <= 15 and length <= 15 and height <= 15 :
+    print(f"Вам нужна коробка № 1.")
+elif length >= 200:
+    print(f"Вам нужна коробка для лыж !")
+elif 15 < width <= 50 and 15 < length <= 50 and 15 < height <= 50:
+    print(f"Вам нужна коробка № 2.")
+else:
+    print(f"Вам нужна коробка № 3.")
+
+
+
+
+
