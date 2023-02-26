bcab1b7d562d6395e25dda264cc096316bf65f24
VAS-PC-IDE <vas.ide2022@gmail.com>
2023-02-26 08:37:57 +0300

    Numpy - start

--- LeetCode/Easy/Roman to Integer.py
+++ LeetCode/Easy/Roman to Integer.py
@@ -7,49 +7,53 @@ class Solution:
 
     def __init__(self, roman_string: str):
         self.roman_string = roman_string
-        self.roman_dict = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XV": 40, "L": 50, "XC": 90, "C": 100,
+        self.roman_dict = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100,
                            "CD": 400, "D": 500, "CM": 900, "M": 1000}
         self.standart_sum = 0
-        self.lst = []
+        self.lst_inf = []
 
-    def roman_to_int(self) -> int:
+    def analiz(self):
         for _, __ in enumerate(self.roman_string):
-            #
-            # Ex:"I"
-            #
-            if __ == "I" and len(self.roman_string) != (_ + 1):
-                if self.roman_string[_ + 1] == "V" or self.roman_string[_ + 1] == "X":
-                    pass
+            if __ == "I":
+                if len(self.roman_string) != _ + 1:
+                    if self.roman_string[_ + 1] == "V" \
+                            or self.roman_string[_ + 1] == "X":
+                        pass
+                    else:
+                        self.lst_inf.append(__)
                 else:
-                    self.lst.append(__)
-            elif __ == "V" and self.roman_string[_ - 1] == "I" or __ == "X" and self.roman_string[_ - 1] == "I":
-                self.lst.append(f"I{__}")
-            #
-            # Ex:"X"
-            #
-            elif __ == "X" and len(self.roman_string) != (_ + 1):
-                if self.roman_string[_ + 1] == "L" or self.roman_string[_ + 1] == "C":
-                    pass
+                    self.lst_inf.append(__)
+            elif __ == "V" and self.roman_string[_ - 1] == "I" and _ != 0\
+                    or __ == "X" and self.roman_string[_ - 1] == "I" and _ != 0:
+                self.lst_inf.append(f"{self.roman_string[_ - 1]}{__}")
+            elif __ == "X":
+                if len(self.roman_string) != _ + 1:
+                    if self.roman_string[_ + 1] == "L" or self.roman_string[_ + 1] == "C":
+                        pass
+                    else:
+                        self.lst_inf.append(__)
                 else:
-                    self.lst.append(__)
-            elif __ == "L" and self.roman_string[_ - 1] == "X" or __ == "C" and self.roman_string[_ - 1] == "X":
-                self.lst.append(f"X{__}")
-            #
-            # Ex:"C"
-            #
-            elif __ == "C" and len(self.roman_string) != (_ + 1):
-                if self.roman_string[_ + 1] == "D" or self.roman_string[_ + 1] == "M":
-                    pass
+                    self.lst_inf.append(__)
+            elif __ == "L" and self.roman_string[_ - 1] == "X" and _ != 0\
+                    or __ == "C" and self.roman_string[_ - 1] == "X" and _ != 0:
+                self.lst_inf.append(f"{self.roman_string[_ - 1]}{__}")
+            elif __ == "C":
+                if len(self.roman_string) != _ + 1:
+                    if self.roman_string[_ + 1] == "D" or self.roman_string[_ + 1] == "M":
+                        pass
+                    else:
+                        self.lst_inf.append(__)
                 else:
-                    self.lst.append(__)
-            elif __ == "D" and self.roman_string[_ - 1] == "C" or __ == "M" and self.roman_string[_ - 1] == "C":
-                self.lst.append(f"C{__}")
-            #
-            # Fin
-            #
+                    self.lst_inf.append(__)
+            elif __ == "D" and self.roman_string[_ - 1] == "C" and _ != 0\
+                    or __ == "M" and self.roman_string[_ - 1] == "C" and _ != 0:
+                self.lst_inf.append(f"{self.roman_string[_ - 1]}{__}")
             else:
-                self.lst.append(__)
-        print(self.lst)
+                self.lst_inf.append(__)
+
+
+
+
 
     # def roman_to_int(self) -> int:
     #     for _, __ in enumerate(self.roman_string):
@@ -95,29 +99,29 @@ class Solution:
     #     return self.standart_sum
 
     def analiz_sum(self):
-        for i in self.lst:
+        print(self.lst_inf)
+        for i in self.lst_inf:
             self.standart_sum += self.roman_dict[i]
         print(f"{self.standart_sum}")
 
-
-
+sol = Solution("MCIV")
+sol.analiz()
+sol.analiz_sum()
 sol = Solution("III")
-sol.roman_to_int()
+sol.analiz()
 sol.analiz_sum()
 sol = Solution("LVIII")
-sol.roman_to_int()
+sol.analiz()
 sol.analiz_sum()
-# Output: 58
 sol = Solution("MCMXCIV")
-sol.roman_to_int()
+sol.analiz()
 sol.analiz_sum()
-# Output: 1994
-
-
 sol = Solution("MMMCDXC")
-sol.roman_to_int()
+sol.analiz()
 sol.analiz_sum()
-sol = Solution("MMMMMCDLXIV")
-sol.roman_to_int()
+sol = Solution("CDLXIX")
+sol.analiz()
 sol.analiz_sum()
-
+sol = Solution("XCII")
+sol.analiz()
+sol.analiz_sum()
